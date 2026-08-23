15e3125ca6d9ba3b8dd2659bacb740769fd5d41b
TobiGamesTalente <tobias.michel@myBub.de>
2026-08-23 21:50:39 +0200

    npc hinzugefügt

--- brackeys-game-jam-2026.2/Scripts/npc.gd
+++ brackeys-game-jam-2026.2/Scripts/npc.gd
@@ -1,20 +1,33 @@
 extends CharacterBody2D
 
-@export var waypoints: Array[Node2D]
-var waypoint_index = 0
-var now_waypoint = waypoints[0]
-	
-	
+const SPEED = 300
 
-func move():
-	pass
+@export var waypoint_container: Node2D
+
+var waypoints: Array[Node]
+var waypoint_index = 0
+
+var now_waypoint: Node2D
+	
+func _ready() -> void:
+	waypoints = waypoint_container.get_children()
+	now_waypoint = waypoints[0]
+	
+func _physics_process(_delta: float) -> void:
+	process_movement()
+	move_and_slide()	
+
+func process_movement():
+
 #	print(global_position.direction_to(waypoint.global_position))
 
-
-	#if global_position.distance_to(waypoint.global_position) > 50:
-	#	velocity = global_position.direction_to(waypoint.global_position) * SPEED
-	#else:
-	#	waypoint = select_other_waypoint()
+	if position.distance_to(now_waypoint.position) > 10:
+	
+		velocity = global_position.direction_to(now_waypoint.global_position) * SPEED
+	else:
+#		print("new waypoint")
+		now_waypoint = select_next_waypoint()
 
 func select_next_waypoint():
-	pass
+	waypoint_index += 1
+	return waypoints[waypoint_index % (len(waypoints) - 1)]
