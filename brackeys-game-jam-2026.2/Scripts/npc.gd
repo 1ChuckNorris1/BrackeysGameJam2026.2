00245048fa66adaadc034a625665f2f0ad7b67ff
TobiGamesTalente <tobias.michel@myBub.de>
2026-08-23 21:09:14 +0200

    improvements

--- brackeys-game-jam-2026.2/Scripts/npc.gd
+++ brackeys-game-jam-2026.2/Scripts/npc.gd
@@ -1,11 +1,20 @@
-extends Node
+extends CharacterBody2D
+
+@export var waypoints: Array[Node2D]
+var waypoint_index = 0
+var now_waypoint = waypoints[0]
+	
+	
+
+func move():
+	pass
+#	print(global_position.direction_to(waypoint.global_position))
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+	#if global_position.distance_to(waypoint.global_position) > 50:
+	#	velocity = global_position.direction_to(waypoint.global_position) * SPEED
+	#else:
+	#	waypoint = select_other_waypoint()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func select_next_waypoint():
 	pass
