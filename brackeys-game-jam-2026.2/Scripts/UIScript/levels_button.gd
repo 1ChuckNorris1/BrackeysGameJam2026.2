82f3225c803db90bad64b45eaa316a395e4bf227
TobiGamesTalente <tobias.michel@myBub.de>
2026-08-24 18:59:41 +0200

    added loose screen

---
+++ brackeys-game-jam-2026.2/Scripts/UIScript/levels_button.gd
@@ -0,0 +1,16 @@
+extends Button
+
+
+@export var link: PackedScene
+
+
+func _ready() -> void:
+	pressed.connect(_change_to_next_scene)
+	update_level_status()
+
+func update_level_status():
+	pass
+
+func _change_to_next_scene():
+	if link == null: return
+	get_tree().change_scene_to_packed(link)
