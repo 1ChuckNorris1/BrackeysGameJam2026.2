7631d4896da9476b42885a5b65d1f1dc9229b909
1ChuckNorris1 <simonklein25@outlook.de>
2026-08-26 21:41:17 +0200

    buttons

--- brackeys-game-jam-2026.2/Scripts/Globals/Gamemanager.gd
+++ brackeys-game-jam-2026.2/Scripts/Globals/Gamemanager.gd
@@ -27,6 +27,7 @@ func loose_game():
 	Global.reset_costumes()
 	get_tree().change_scene_to_packed(LOOSE_SCREEN)
 	
+
 func win_game(level_name: String):
 	Global.win_level(level_name)
 	Global.reset_costumes()
